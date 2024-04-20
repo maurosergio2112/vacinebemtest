8441696f22ee56457a7d5f8e3e81dbd4a4573783
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-04-19 23:56:15 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 const port = 3000;
 
 app.get("/teste", (req, res) => {
